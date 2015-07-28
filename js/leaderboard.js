cac5dfcb13392c691510a41fdb1cfe12d24b24fe
rodekruis <mvanderveen@redcross.nl>
2015-07-28 16:01:24 +0200

    update benin page

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,7 +1,7 @@
 (function(){
   // extend app w/ map module
   $.extend(app, {
-    osmHistoryBaseURL: 'https://api.developmentseed.org/osm?search=comment:%22missingmaps%22&limit=100&skip=1',
+    osmHistoryBaseURL: 'https://api.developmentseed.org/osm?search=comment:%22missingmaps%22&limit=1000&skip=0',
     blacklist: ['JamesLC'],
     contributorGeoJSONLayer: null,
     loadingContributorGeoJSON: false,
